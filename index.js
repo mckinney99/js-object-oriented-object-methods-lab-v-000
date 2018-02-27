5585252378f5aa7874c8d7f11ed4a558e515645e
Eric McKinney <mckinney99@gmail.com>
2018-02-27 16:50:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ function BoardMember (name, homeState, training){
   this.homeState = homeState
   this.training = training
   this.veto = function(){
-    return("No, I must disagree")
+    console.log("No, I must disagree")
   }
   this.approve = function(){
-    return("You can do that!")
+    console.log("You can do that!")
   }
   this.doCharity = function(){
-    return("I like to help people")
+    console.log("I like to help people")
   }
   this.releasePressStatement = function(){
-    return("You will see great things from Scuber")
+    console.log("You will see great things from Scuber")
   }
   this.sayHi = function(){
-    return("Hi, my name is "${this.name}". I am from "${this.homeState}", and I was trained in "${this.training}".")
+    console.log("Hi, my name is "${this.name}". I am from "${this.homeState}", and I was trained in "${this.training}".")
   }
 }
-
